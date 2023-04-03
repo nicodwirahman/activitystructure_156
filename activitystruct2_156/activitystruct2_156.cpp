0ea6c4f50498762f5fa7c0dd7b03a3e8ba91f30e
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-04-03 14:53:29 +0700

    cout

---
+++ activitystruct2_156/activitystruct2_156.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+struct alamatDetail
+{
+	string desa;
+	string kota;
+
+
+};
+
+struct mahasiswa {
+	string nim;
+	string nama;
+	alamatDetail alamat;
+	int umur;
+};
+
+int main() {
+
+	mahasiswa mhs;
+}
