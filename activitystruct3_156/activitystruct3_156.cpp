c49fe1a0d67021229baf67a964d4e62aaea585df
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-04-03 15:30:29 +0700

    struct3

--- activitystruct3_156/activitystruct3_156.cpp
+++ activitystruct3_156/activitystruct3_156.cpp
@@ -21,6 +21,26 @@ int main() {
 	for (int i = 0; i < 3; i++)
 	{
 		cout << "masukkan nim: ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "masukkan nama :";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat :" << endl;
+		cout << "\tNama desa :";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\tNama kota :";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "masukkan umur :";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
 	}
 
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\nNim : " << mhs[i].nim;
+		cout << "\nNama : " << mhs[i].nama;
+		cout << "\ndesa : " << mhs[i].alamat.desa;
+		cout << "\kota : " << mhs[i].alamat.kota;
+		cout << "\numur: " << mhs[i].umur;
+	}
 }
+
